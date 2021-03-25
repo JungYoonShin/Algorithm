9f9006d5f62e5babfdf3a0d13f9047836ad4628e
JungYoonShin <63058347+JungYoonShin@users.noreply.github.com>
2021-03-25 17:09:25 +0900

    Update 1085.py

--- 1085.py
+++ 1085.py
@@ -1,5 +1,7 @@
 x, y, w, h = map(int, input().split())
 num = [x, y, w-x, h-y]
+# num = [0] * 4
+# num[0], num[1], num[2], num[3] = x, y, w-x, h-y
 min = num[0]
 for i in range(1,4):
     if num[i] < min:
