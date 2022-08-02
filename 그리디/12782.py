7268e667bf1ae2dd9160782886bd9881a9622a2c
JungYoonshin <sju08227@naver.com>
2022-08-02 13:46:43 +0900

    [Baekjoon] 그리디- 12782번

--- 그리디/12782.py
+++ 그리디/12782.py
@@ -1,25 +1,25 @@
-#12782번
-# n = int(input())
+# 12782번
+n = int(input())
 
-# for _ in range(n):
-#     a, b = input().split()
-#     count_1 = 0
-#     count_0 = 0
-#     sum = 0
-#     for i in range(len(a)):
-#         #교환
-#         if a[i] != b[i]:
-#             if b[i] == '1':
-#                 count_1 += 1
-#             else:
-#                 count_0 += 1
-#         if count_1 >= 1 and count_0 >= 1:
-#             sum += 1
-#             count_1 -= 1
-#             count_0 -= 1
-#     if count_0 > 0 or count_1 > 0:
-#         sum += count_0 + count_1
-#     print(sum)
+for _ in range(n):
+    a, b = input().split()
+    count_1 = 0
+    count_0 = 0
+    sum = 0
+    for i in range(len(a)):
+        #교환
+        if a[i] != b[i]:
+            if b[i] == '1':
+                count_1 += 1
+            else:
+                count_0 += 1
+        if count_1 >= 1 and count_0 >= 1:
+            sum += 1
+            count_1 -= 1
+            count_0 -= 1
+    if count_0 > 0 or count_1 > 0:
+        sum += count_0 + count_1
+    print(sum)
 
 # 3
 # 1011 1100
