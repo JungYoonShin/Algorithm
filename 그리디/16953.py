3d744beba8207a28215481c2308c0806b50c7238
JungYoonShin <63058347+JungYoonShin@users.noreply.github.com>
2023-04-20 21:58:11 +0900

    [Baekjoon] 16953 - "A → B"

---
+++ 그리디/16953.py
@@ -0,0 +1,48 @@
+#A → B
+# 정수 A를 B로 바꾸려고 한다. 가능한 연산은 다음과 같은 두 가지이다.
+
+# 2를 곱한다.
+# 1을 수의 가장 오른쪽에 추가한다.
+# A를 B로 바꾸는데 필요한 연산의 최솟값을 구해보자.
+import sys
+
+f = sys.stdin.readline
+a, b = map(int, f().split())
+
+result = 1
+while (b!=a):
+  temp = b
+  if(b % 10 == 1):
+    b //= 10
+  elif(b % 2 == 0):
+    b //= 2
+  result += 1
+
+  if(temp == b):
+    print(-1)
+    break
+  
+else:
+  print(result)
+
+
+#시간 초과 뜬 코드..
+# import sys
+
+# f = sys.stdin.readline
+# a, b = map(int, f().split())
+
+# result = 1
+# while (b!=a):
+#   if(b % 10 == 1):
+#     b //= 10
+#   elif(b % 2 == 0):
+#     b //= 2
+#   result += 1
+
+#   if(b <= a and b != a):
+#     print(-1)
+#     break
+  
+# else:
+#   print(result)
