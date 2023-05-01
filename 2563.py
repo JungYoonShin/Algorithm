3fa924ee8cd86b9703da30ccbc3dde91c6c146f5
JungYoonShin <63058347+JungYoonShin@users.noreply.github.com>
2023-05-01 15:24:29 +0900

    [Baekjoon] 2563번 코드

---
+++ 2563.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+paper = [[0]*101 for i in range(101)]
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    for i in range(10):
+        for j in range(10):
+            paper[a+i][b+j] = 1
+result = 0
+for i in paper:
+    result += sum(i)
+print(result)
+    
