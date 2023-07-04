80da164e8d253f1c86fa566ce06f1d0a87a3b528
JungYoonshin <sju08227@naver.com>
2023-07-04 07:57:20 +0000

    [Baekjoon] 그리디+정렬 - 1461번 코드

--- 정렬/1461.py
+++ 정렬/1461.py
@@ -1,4 +1,5 @@
 import sys
+
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
@@ -14,20 +15,19 @@ for book in books:
     plus.append(book)
 
 #가장 먼 위치의 책은 맨 마지막에 제자리에 둔다.(0으로 돌아올 필요 없으므로)
-# max_far = abs(books[0]) if abs(books[0]) > abs(books[-1]) else max_far = abs(books[-1])
 location = []
 for i in range(0, len(minus), m):
   location.append(abs(minus[i]))
   if i + m > len(minus):
-    break  
+    break
 
-for i in range(len(plus)-1, -1, -m):
+for i in range(len(plus) - 1, -1, -m):
   location.append(plus[i])
   if i - m < 0:
-    break  
-    
-location.sort(reverse= True)
-result = location[0] + sum(location[1:])*2
+    break
+
+location.sort(reverse=True)
+result = location[0] + sum(location[1:]) * 2
 print(result)
 
 ## 정답률이 높아서 풀었는데 이 문제도 그리 쉽지는 않았다,,, (그리디 + 정렬)
