cf255eacdc5315b07184569cf7d300a68ab1c63f
JungYoonShin <sju08227@naver.com>
2023-10-23 08:27:33 +0000

    [Baekjoon] 1️⃣ 14891번 다시풀기

--- main.py
+++ main.py
@@ -1,36 +1,31 @@
 import sys
 from collections import deque
-
 input = sys.stdin.readline
 
-
-def right_rotate(num, direction):
-  if num > 4:
+def rotate_right(i, d):
+  if i > 4:
     return
-  if graph[num - 1][2] != graph[num][6]:
-    right_rotate(num + 1, -direction)
-    graph[num].rotate(-direction)
+  if graph[i][6] != graph[i-1][2]:
+    rotate_right(i+1, -d)
+    graph[i].rotate(-d)
 
-
-def left_rotate(num, direction):
-  if num < 1:
+def rotate_left(i, d):
+  if i < 1:
     return
-  if graph[num][2] != graph[num + 1][6]:
-    left_rotate(num - 1, -direction)
-    graph[num].rotate(-direction)
+  if graph[i][2] != graph[i+1][6]:
+    rotate_left(i-1, -d)
+    graph[i].rotate(-d)
 
-
-graph = [0] * (10 ^ 20)
+graph = [0] * (10^20)
 for i in range(1, 5):
   graph[i] = deque((map(int, input().rstrip())))
-
 rotate_num = int(input())
 for _ in range(rotate_num):
-  num, direction = map(int, input().split())
-  right_rotate(num + 1,direction)
-  left_rotate(num - 1, direction)
-  graph[num].rotate(direction)
+  n, d = map(int, input().split())
+  rotate_right(n+1, d)
+  rotate_left(n-1, d)
+  graph[n].rotate(d)
 result = 0
 for i in range(4):
-  result += graph[i+1][0] * (2**i)
+  result += graph[i + 1][0] * (2**i)
 print(result)
